5b907ca02bdc0fff981c947aaec16bec689214cd
olasesan694 <olasesan694@gmail.com>
2020-08-04 21:25:30 -0400

    embed socials

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -8,14 +8,14 @@
                 <p> <i class="fa fa-dashboard" aria-hidden="true"></i> <a href="dashboard"> Dashboard </a></p>
                 <p> <i class="fa fa-address-book-o" aria-hidden="true"></i> <a href="register">Register </a> </p>
                 <p> <i class="fa fa-sign-in" aria-hidden="true"></i> <a href="login">Login </a> </p>
-                <p> <i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="#footer">support </a> </p>
+                <p> <i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="contact">Contact </a> </p>
             </div>
             <div class="col-md-4 footer-box">
                 <p> <b>Resources</b> </p>
-                <p> <i class="fa fa-file" aria-hidden="true"></i> <a href="docs">Docs & APIs </a> </p>
+                <p> <i class="fa fa-file-o" aria-hidden="true"></i> <a href="docs">Docs & APIs </a> </p>
                 <p> <i class="fa fa-dollar" aria-h idden="true"></i> <a href="pricing">Pricing </a> </p>
                 <p> <i class="fa fa-mortar-board" aria-hidden="true"></i> <a href="jobs">Jobs </a> </p>
-                <p> <i class="fa fa-envelope" aria-hidden="true"></i> <a href="contact">Contact </a> </p>
+                <p> <i class="fa fa-file-o" aria-hidden="true"></i> <a href="blog">Blog </a> </p>
             </div>
             <div class="col-md-4 footer-box">
                 <p><b> <a href="#footer">Sunnyvale, California 94043 </a> </b> </p>
